206ad5565bebf61a9505457a1e47f6ed66d5d2cd
it24102709 <it24102709@my.sliit.lk>
2024-10-04 07:05:26 +0530

    Add files via upload

---
+++ Q2/IT24102709Lab7Q2C.java
@@ -0,0 +1,11 @@
+public class IT24102709Lab7Q2C {
+	public static void main(String[] args) {
+	
+		for(int x = 5; x >= 1; 1--) {
+		for (int y = 1; y <= i; j++) {
+			System.out.print(i);
+		}
+		System.out.println();
+		}
+	}
+}
